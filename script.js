7cfb415509d5099a269a1f3152360e4afb1d819e
mk42jk <133700925+mk42jk@users.noreply.github.com>
2024-05-18 19:08:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function randomImage() {
-    var images = [
-        { src: "img/butterfly.png", title: "papillon" },
-        { src: "img/car.png", title: "voiture" },
-        { src: "img/cat.png", title: "chat" },
-        { src: "img/cow.png", title: "vache" },
-        { src: "img/flower.png", title: "fleur" },
-        { src: "img/tree.png", title: "arbre" }
-    ];
 
-    var randomIndex = Math.floor(Math.random() * images.length);
-    var randomImage = images[randomIndex];
-    document.getElementById("randomImage").src = randomImage.src;
-    document.getElementById("randomImage").title = randomImage.title;
-}
 
     function randomImage() {
       var images = [
